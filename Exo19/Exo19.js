0d3b23b15b09cbc1d17466d78c4062f059191276
Emeric-Maximil <maximil.emeric@gmail.com>
2023-11-20 12:09:13 +0100

    exo 19 sur les query

---
+++ Exo19/Exo19.js
@@ -0,0 +1,6 @@
+document.querySelector('h1').style.fontSize = 123 + 'px';
+document.querySelector('h1').textContent = 'nouveau titre';
+document.querySelector('div').style.fontFamily = 'Arial';
+document.querySelector('div').style.marginTop = '30px';
+document.querySelector('p').style.color = 'green';
+document.querySelector('p').textContent = 'nouveau contenu du paragraphe.';

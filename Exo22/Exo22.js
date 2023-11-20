89dcc7176822c35f415d521eaecde7e2d3abf998
Emeric-Maximil <maximil.emeric@gmail.com>
2023-11-20 16:50:26 +0100

    exo 22 sur le prix TTC

---
+++ Exo22/Exo22.js
@@ -0,0 +1,26 @@
+let compteur = 1;
+
+
+function calculer() {
+    let valeur = document.getElementById("quantite").value;
+
+    if(document.querySelectorAll("p").length == 4){
+        
+        console.log("il y a 4 paragraphes");
+        let total = 0;
+
+        let p1 = document.querySelectorAll('p')[2].textContent;
+        let p2 = document.querySelectorAll('p')[3].textContent;
+        total = p1 * p2 * 1.2;
+
+        console.log(total);
+        document.getElementById("resultat").textContent = "Le prix TTC est de "+document.querySelectorAll('p')[1].textContent+" est de "+total;
+    }else {
+        console.log(compteur);
+        document.body.appendChild(document.createElement('p'));
+        document.querySelectorAll('p')[compteur].textContent = valeur;
+        compteur++;
+    }
+
+    
+}

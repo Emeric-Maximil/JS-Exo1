16606b759195852c712e24ebb024531b3c1b4fbf
Emeric-Maximil <maximil.emeric@gmail.com>
2023-11-22 09:51:48 +0100

    exo 26 mais corrigé par mes SOINS

--- Exo26/Exo26.js
+++ Exo26/Exo26.js
@@ -1,47 +1,49 @@
 let statut = true;
 
 function addTask() {
-    let div = document.createElement("div");
-    div.setAttribute("class", "container");
+    if (document.getElementById("task").value != "") {
+        let div = document.createElement("div");
+        div.setAttribute("class", "container");
 
-    let para = document.createElement("p");
-    para.textContent = document.getElementById("task").value;
+        let para = document.createElement("p");
+        para.textContent = document.getElementById("task").value;
 
-    let button1 = document.createElement("button");
-    button1.setAttribute("id", "delete");
-    button1.setAttribute("onclick", "deleteTask(this)");
-    button1.textContent = "delete";
+        let button1 = document.createElement("button");
+        button1.setAttribute("id", "delete");
+        button1.setAttribute("onclick", "deleteTask(this)");
+        button1.textContent = "delete";
 
-    let button2 = document.createElement("button");
-    button1.setAttribute("id", "update");
-    button2.setAttribute("onclick", "updateTask(this)");
-    button2.textContent = "update";
+        let button2 = document.createElement("button");
+        button1.setAttribute("id", "update");
+        button2.setAttribute("onclick", "updateTask(this)");
+        button2.textContent = "update";
 
-    div.appendChild(para);
-    div.appendChild(button1);
-    div.appendChild(button2);
+        div.appendChild(para);
+        div.appendChild(button1);
+        div.appendChild(button2);
 
-    document.body.appendChild(div);
+        document.getElementById("tasks").appendChild(div);
 
-    //le code ci-dessous  : récupére la clé tasks dans le localstorage
-    //et le met à jour avec la valeur saisie dans l'input du formulaire
-    //récupération de l'input ('#task)
-    let valeur = document.querySelector("#task").value;
-    //récupérer dans une variable la valeur clé ('tasks')
-    let tasks = localStorage.getItem("tasks");
-    //test si la clé tasks dans localstorage est vide
-    if (tasks == "") {
-        tasks += valeur;
+        //le code ci-dessous  : récupére la clé tasks dans le localstorage
+        //et le met à jour avec la valeur saisie dans l'input du formulaire
+        //récupération de l'input ('#task)
+        let valeur = document.querySelector("#task").value;
+        //récupérer dans une variable la valeur clé ('tasks')
+        let tasks = localStorage.getItem("tasks");
+        //test si la clé tasks dans localstorage est vide
+        if (tasks == "") {
+            tasks += valeur;
+        }
+        //test sinon elle n'est pas vide
+        else {
+            //transforme en tableau
+            tasks = tasks.split(",");
+            //ajoute la valeur de l'input au tableau
+            tasks.push(valeur);
+        }
+        //mise à jour de la clé
+        localStorage.setItem("tasks", tasks);
     }
-    //test sinon elle n'est pas vide
-    else {
-        //transforme en tableau
-        tasks = tasks.split(",");
-        //ajoute la valeur de l'input au tableau
-        tasks.push(valeur);
-    }
-    //mise à jour de la clé
-    localStorage.setItem("tasks", tasks);
 }
 
 function delAllTask() {
@@ -83,37 +85,36 @@ localStorage.getItem("tasks") == null
     ? localStorage.setItem("tasks", [])
     : false;
 
+function showAllTask() {
+    let tasks = localStorage.getItem("tasks").split(",");
 
-function showAllTask(){
-    let tasks = localStorage.getItem('tasks').split(',');
+    for (let i = 0; i < tasks.length; i++) {
+        if (tasks[i] != "") {
+            let div = document.createElement("div");
+            div.setAttribute("class", "container");
 
-    for(let i = 0; i< tasks.length; i++){
-        let div = document.createElement("div");
-        div.setAttribute("class", "container");
+            let para = document.createElement("p");
+            para.textContent = tasks[i];
 
-        let para = document.createElement("p");
-        para.textContent = tasks[i];
+            let button1 = document.createElement("button");
+            button1.setAttribute("id", "delete");
+            button1.setAttribute("onclick", "deleteTask(this)");
+            button1.textContent = "delete";
 
-        let button1 = document.createElement("button");
-        button1.setAttribute("id", "delete");
-        button1.setAttribute("onclick", "deleteTask(this)");
-        button1.textContent = "delete";
+            let button2 = document.createElement("button");
+            button1.setAttribute("id", "update");
+            button2.setAttribute("onclick", "updateTask(this)");
+            button2.textContent = "update";
 
-        let button2 = document.createElement("button");
-        button1.setAttribute("id", "update");
-        button2.setAttribute("onclick", "updateTask(this)");
-        button2.textContent = "update";
+            div.appendChild(para);
+            div.appendChild(button1);
+            div.appendChild(button2);
 
-        div.appendChild(para);
-        div.appendChild(button1);
-        div.appendChild(button2);
-
-        document.body.appendChild(div);
+            document.getElementById('tasks').appendChild(div);
+        }
     }
-    
-//________________
-    
+}
 
-    
-
+function viderLocalStorage(){
+    localStorage.clear();
 }
